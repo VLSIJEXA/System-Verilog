307219b39098e2961a14bde2b80bb21bff446542
JATIN KATIYAR <katiyarjatin943@gmail.com>
2025-03-03 08:00:58 +0530

    Update unpacked_array.sv

--- unpacked_array.sv
+++ unpacked_array.sv
@@ -2,7 +2,6 @@ timescale 1ns/100ps
 module array_literals ();
 
 byte a [0:2][0:2] = '{'{1,2,3}, '{5,6,7}, '{8,9,10}};
-
 initial begin
   $display ("a [0][0] = %d", a[0][0]);
   $display ("a [0][1] = %d", a[0][1]);
