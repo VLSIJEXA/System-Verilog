719479a78698af75b603272d479c3b7ba9613727
JATIN KATIYAR <katiyarjatin943@gmail.com>
2025-03-12 11:02:37 +0530

    Update use_dynamic_array.sv

--- use_dynamic_array.sv
+++ use_dynamic_array.sv
@@ -66,6 +66,33 @@ output
 # KERNEL: arr: 0 5 10 15 20 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
 
 ///if you want to store your date in fix size array then you need to match eject size of array and same datatype also
+module tb;
+ 
+int arr[];  
+  int farr[30];  //fixed array ,you need to match eject size of array  and same datatype also
   
+ initial begin
+   
+   arr=new[5]; 
+   
+   for(int i=0;i<5;i++) begin
+     arr[i]=5*i;
+   end
+     $display("arr: %0p", arr);
+      arr=new[30](arr);
+   $display("arr: %0p", arr);
+   farr=arr;
+   $display("farr: %0p", farr);
+  
+ end
+   initial 
+    begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+  end
+endmodule
+output  
+KERNEL: arr: 0 5 10 15 20 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+# KERNEL: farr: 0 5 10 15 20 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
 
 
